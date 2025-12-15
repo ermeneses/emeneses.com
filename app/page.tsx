8b382b2fd3bce6e8b07218efa4930473017bd8dd
ermeneses <ermenesesdenis@gmail.com>
2025-12-15 17:01:57 -0500

    feat(profile): update avatar and personal details

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+/* eslint-disable @next/next/no-img-element */
+export default function Page() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-medium">Eduardo Meneses Denis</h1>
+    <div className="w-full h-svh flex flex-col justify-center items-center gap-4 flex-wrap">
+      <div className="w-32 h-32 overflow-hidden rounded-xl relative">
+        <img src="/yo.png" alt="yo" className="w-32 absolute -top-5" />
+      </div>
+      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+        Eduardo Meneses Denis
+      </h4>
     </div>
   );
 }
